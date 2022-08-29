7d05684062cf95c2114e8d24f0f810eac9ec99ac
Andreas Kempe <andreas.kempe@byte-artist.de>
2022-08-29 10:46:57 +0200

    remove not needed type qualifiers in applicationRules.h

--- applicationRules.h
+++ applicationRules.h
@@ -7,9 +7,9 @@ class ApplicationRules {
     
     public:
         ApplicationRules(const bool isRequired = false, const QString defaultValue = "", const bool isIgnored = false);
-        const bool isRequired() {return this->m_isRequired;}
-        const QString getDefaultValue() {return m_defaultValue;}
-        const bool isIgnored() {return this->m_isIgnored;}
+        bool isRequired() {return this->m_isRequired;}
+        QString getDefaultValue() {return m_defaultValue;}
+        bool isIgnored() {return this->m_isIgnored;}
 
     private:
         bool m_isRequired;
