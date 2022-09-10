4b0a7d06fbad3ca86f6ae1234647a443a9627853
Andreas Kempe <andreas.kempe@byte-artist.de>
2022-09-10 11:40:01 +0200

    write test for buttonmanager, remove build dir from repo

--- tests/tst_buttonManager.cpp
+++ tests/tst_buttonManager.cpp
@@ -1,9 +1,36 @@
 #include "tst_buttonManager.h"
 
+#include "buttonManager.h"
+#include "application.h"
+
 #include <QTest>
 
 TestButtonManager::TestButtonManager(QObject *parent)
     : QObject{parent}
 {
 
+}
+
+void TestButtonManager::generate() {
+    Applications* applications = new Applications();
+    ButtonManager* buttonManager = new ButtonManager(applications);
+    Application* application = new Application();
+
+    QPushButton* button = buttonManager->generate(application);
+}
+
+void TestButtonManager::generate_data() {
+    QTest::addColumn<QString>("path");
+    QTest::addColumn<QString>("name");
+    QTest::addColumn<QString>("iconPath");
+    QTest::addColumn<Applications*>("applications");
+    QTest::addColumn<uint>("expectedCount");
+
+    QTest::newRow("single screen resolution") << QRect(0, 0, 1920, 1080) << generateFakeApplicationCollection(0) << QRect(913, 0, 94, 94);
+}
+
+Applications* TestButtonManager::generateApplications(QString path) {
+    if (!path.isEmpty()) {
+
+    }
 }
--- tests/tst_buttonManager.h
+++ tests/tst_buttonManager.h
@@ -1,6 +1,8 @@
 #ifndef TESTBUTTONMANAGER_H
 #define TESTBUTTONMANAGER_H
 
+#include "applications.h"
+
 #include <QObject>
 
 class TestButtonManager : public QObject
@@ -8,12 +10,16 @@ class TestButtonManager : public QObject
     Q_OBJECT
 
 private slots:
+    void generate();
+    void generate_data();
 
 public:
     explicit TestButtonManager(QObject *parent = nullptr);
 
 signals:
 
+private:
+    Applications* generateApplications(QString path = "");
 };
 
 #endif // TESTBUTTONMANAGER_H
